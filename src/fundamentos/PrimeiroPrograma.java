1edaa24f511d05517d28435cdde5647fea0a4b1c
Joao Vinicio <joaovinicioans@gmail.com>
2026-02-07 17:00:33 -0300

    estrutura inicial da classe disciplina

--- src/fundamentos/PrimeiroPrograma.java
+++ src/fundamentos/PrimeiroPrograma.java
@@ -2,7 +2,17 @@ package fundamentos;
 
 public class PrimeiroPrograma {
     public static void main(String[] args) {
-        System.out.println("Olá Mundo");
+
+        // Uma sentença de código termina com ;
+
+        System.out.println("Primeiro Programa Parte 01#");
+        System.out.println("Primeiro Programa Parte 02#");
+
+        /*
+        * Comentarios de Multiplas linhas
+        * Enquanto não encontrar o asterico / o comentario
+        * Não Termina.
+        * */
 
     }
 }
