bc477869d27bdfe57fb9f5936acc2f4118bf04bd
Joao Vinicio <joaovinicioans@gmail.com>
2026-02-10 21:10:53 -0300

    Estudo sobre gerenciamento de imports e organização de pacotes com IntelliJ

---
+++ src/fundamentos/Import.java
@@ -0,0 +1,23 @@
+package fundamentos;
+/**
+ * Exercício: Gerenciamento de Imports
+ * O que aprendi:
+ * - Organização Profissional: O comando Ctrl + Alt + O mantém o código limpo,
+ * removendo referências desnecessárias que poluem o cabeçalho do arquivo.
+ * - Conflitos de Nomes: Entendi que o import resolve ambiguidades, permitindo
+ * que o Java saiba exatamente qual biblioteca usar (ex: java.util.Date).
+ */
+import java.util.Date;
+
+public class Import {
+    public static void main(String[] args) {
+
+        String s = "Bom dia";
+        System.out.println(s);
+
+        Date d = new Date();
+        System.out.println(d);
+
+
+    }
+}
