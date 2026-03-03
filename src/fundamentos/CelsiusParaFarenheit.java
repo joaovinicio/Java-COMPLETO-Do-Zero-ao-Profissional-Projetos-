e03c1b5129a7fced4897fa7e9f72a5e45ab2a9de
Joao Vinicio <joaovinicioans@gmail.com>
2026-03-02 21:59:06 -0300

    Implementa conversor de Celsius para Fahrenheit

---
+++ src/fundamentos/CelsiusParaFarenheit.java
@@ -0,0 +1,22 @@
+package fundamentos;
+
+/**
+ * EXERCÍCIO: Conversão de Celsius para Fahrenheit.
+ * - Objetivo: Demonstrar a aplicação da fórmula (C * 9/5) + 32.
+ * - Aprendizado: Uso de variáveis de ponto flutuante (double) para manter precisão
+ * e formatação de saída com printf.
+ * - Resultado obtido: 145°C convertidos com sucesso para 293°F.
+ */
+
+public class CelsiusParaFarenheit {
+    public static void main(String[] args) {
+
+        double ajuste = 32;
+        double fator = 9.0 / 5.0;
+        double temperaturaCelsius = 42;
+
+        double temperatura = (temperaturaCelsius * fator) + ajuste;
+        System.out.printf("A temperatura de Celsius para Farenheit é : %.2f %s ",temperatura , "ºF");
+
+    }
+}
