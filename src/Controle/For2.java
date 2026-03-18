b0d907571759455096b340e75eeedaed71c29fe3
Joao Vinicio <joaovinicioans@gmail.com>
2026-03-18 14:46:34 -0300

    Estudo de Variações da Estrutura de Repetição For. limpa warnings do IntelliJ e organiza exemplos de laço for

--- src/Controle/For2.java
+++ src/Controle/For2.java
@@ -9,19 +9,16 @@ package Controle;
 public class For2 {
     public static void main(String[] args) {
 
-        /**
-         * Desafio criar um laço for Decrescente de 2 em 2*/
-
         for(int contador =10; contador >=0; contador -=2 ){
             System.out.printf("Contador : %d %n",contador);
         }
-        System.out.println("");
+        System.out.println();
         int conta;
         for(conta =10; conta >=0; ){
             System.out.printf("Conta : %d %n",conta);
             conta -=2;
         }
-        System.out.println("");
+        System.out.println();
         for(int conta2 =12; conta2>=0; ){
             System.out.printf("Conta 2: %d %n",conta2);
             conta2 -=2;
