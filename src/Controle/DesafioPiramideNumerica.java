2e2f850140770da4ff68577a4d2bcdb1e38ef370
Joao Vinicio <joaovinicioans@gmail.com>
2026-03-18 17:03:13 -0300

    desafio de pirâmide numérica com controle de quebra de linha

---
+++ src/Controle/DesafioPiramideNumerica.java
@@ -0,0 +1,14 @@
+package Controle;
+
+public class DesafioPiramideNumerica {
+    public static void main(String[] args) {
+
+        for(int linha = 1; linha <=5; linha++){
+            for(int coluna =1; coluna <=linha; coluna++){
+                System.out.printf("%d",linha);
+            }
+            System.out.println();
+        }
+
+    }
+}
